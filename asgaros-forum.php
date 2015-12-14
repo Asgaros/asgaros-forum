49fcc80fd87f2c9c8fbb59ec30d3deca4eac8d56
Thomas <developer@thomasbelser.net>
2015-12-14 19:13:33 +0100

    Changed plugin meta information
    Changed plugin meta information in preparation for the 1.0.0 release.

--- asgaros-forum.php
+++ asgaros-forum.php
@@ -4,26 +4,26 @@
   Plugin Name: Asgaros Forum
   Plugin URI: https://github.com/Asgaros/asgaros-forum
   Description: A lightweight and simple forum plugin for WordPress.
-  Version: 1.0.0 Development-Version
+  Version: 1.0.0
   Author: Thomas Belser
-  Author URI: https://github.com/Asgaros/asgaros-forum
+  Author URI: https://thomasbelser.net
+  License: GPL2
+  License URI: https://www.gnu.org/licenses/gpl-2.0.html
   Text Domain: asgarosforum
 
-  GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
-  This program is free software; you can redistribute it and/or modify
+  Asgaros Forum is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  the Free Software Foundation, either version 2 of the License, or
+  any later version.
 
-  This program is distributed in the hope that it will be useful,
+  Asgaros Forum is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+  along with Asgaros Forum. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
 
 //Textdomain Hook
 $plugin_dir = plugin_basename(dirname(__FILE__));
@@ -33,7 +33,7 @@ load_plugin_textdomain('asgarosforum', false, $plugin_dir . '/languages/');
 define('WPAFURL', plugin_dir_url(__FILE__));
 
 //Load class files
-require("forum.php");
+require('forum.php');
 
 if (is_admin()) {
     require_once(dirname(__FILE__).'/admin/admin.php');
