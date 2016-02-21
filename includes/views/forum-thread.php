af06e5cf1ccff734f7dc5453adcbe5b997072574
Thomas <developer@thomasbelser.net>
2016-02-22 00:22:37 +0100

    Combined code for forum view
    Combined code for forum view (sticky and normal thread listing).

---
+++ includes/views/forum-thread.php
@@ -0,0 +1,12 @@
+<div class="thread">
+    <div class="thread-status"><?php $this->get_thread_image($thread->id, $thread->status); ?></div>
+    <div class="thread-name">
+        <strong><a href="<?php echo $this->get_link($thread->id, $this->url_thread); ?>"><?php echo $this->cut_string($thread->name); ?></a></strong>
+        <small><?php _e('Created by:', 'asgaros-forum'); ?> <i><?php echo $this->get_username($this->get_thread_starter($thread->id)); ?></i></small>
+    </div>
+    <div class="thread-stats">
+        <small><?php _e('Answers', 'asgaros-forum'); ?>: <?php echo (int) ($this->count_elements($thread->id, $this->table_posts) - 1); ?></small>
+        <small><?php _e('Views', 'asgaros-forum'); ?>: <?php echo (int) $thread->views; ?></small>
+    </div>
+    <div class="thread-poster"><?php echo $this->get_lastpost_in_thread($thread->id); ?></div>
+</div>
--- includes/views/forum.php
+++ includes/views/forum.php
@@ -23,39 +23,18 @@ if (!is_user_logged_in()) {
     <div class="content-element">
         <?php if ($sticky_threads && !$this->current_page) { ?>
             <div class="bright"><?php _e('Sticky Threads', 'asgaros-forum'); ?></div>
-            <?php foreach ($sticky_threads as $thread) { ?>
-                <div class="thread">
-                    <div class="thread-status"><?php $this->get_thread_image($thread->id, $thread->status); ?></div>
-                    <div class="thread-name">
-                        <strong><a href="<?php echo $this->get_link($thread->id, $this->url_thread); ?>"><?php echo $this->cut_string($thread->name); ?></a></strong>
-                        <small><?php _e('Created by:', 'asgaros-forum'); ?> <i><?php echo $this->get_username($this->get_thread_starter($thread->id)); ?></i></small>
-                    </div>
-                    <div class="thread-stats">
-                        <small><?php _e('Answers', 'asgaros-forum'); ?>: <?php echo (int) ($this->count_elements($thread->id, $this->table_posts) - 1); ?></small>
-                        <small><?php _e('Views', 'asgaros-forum'); ?>: <?php echo (int) $thread->views; ?></small>
-                    </div>
-                    <div class="thread-poster"><?php echo $this->get_lastpost_in_thread($thread->id); ?></div>
-                </div>
-            <?php }
+            <?php foreach ($sticky_threads as $thread) {
+                require('forum-thread.php');
+            }
+
             if ($counter_normal > 0) { ?>
                 <div class="bright"></div>
             <?php }
         }
 
-        foreach ($threads as $thread) { ?>
-            <div class="thread">
-                <div class="thread-status"><?php $this->get_thread_image($thread->id, $thread->status); ?></div>
-                <div class="thread-name">
-                    <strong><a href="<?php echo $this->get_link($thread->id, $this->url_thread); ?>"><?php echo $this->cut_string($thread->name); ?></a></strong>
-                    <small><?php _e('Created by:', 'asgaros-forum'); ?> <i><?php echo $this->get_username($this->get_thread_starter($thread->id)); ?></i></small>
-                </div>
-                <div class="thread-stats">
-                    <small><?php _e('Answers', 'asgaros-forum'); ?>: <?php echo (int) ($this->count_elements($thread->id, $this->table_posts) - 1); ?></small>
-                    <small><?php _e('Views', 'asgaros-forum'); ?>: <?php echo (int) $thread->views; ?></small>
-                </div>
-                <div class="thread-poster"><?php echo $this->get_lastpost_in_thread($thread->id); ?></div>
-            </div>
-        <?php } ?>
+        foreach ($threads as $thread) {
+            require('forum-thread.php');
+        } ?>
     </div>
 
     <div>
