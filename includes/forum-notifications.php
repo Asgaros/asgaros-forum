f79ff80f2825f4901609097c5684e28ee9cb7369
Thomas <developer@thomasbelser.net>
2016-04-30 17:13:52 +0200

    Adding subscription-check for current topic
    Adding a subscription-check for the current topic. The
    (un)subscribe-link uses this function.
    
    Reference: #22

--- includes/forum-notifications.php
+++ includes/forum-notifications.php
@@ -25,19 +25,31 @@ class AsgarosForumNotifications {
         if ($asgarosforum->options['allow_subscriptions'] && is_user_logged_in()) {
             echo '<div id="topic-subscription">';
 
-            // User has no subscription for this topic
-            if (true) {
-                echo '<a href="#">';
-                _e('<b>Subscribe</b> to this topic.', 'asgaros-forum');
-                echo '</a>';
-            } else {
+            if (self::isSubscribed($asgarosforum->current_thread)) {
                 // User has subscription for this topic
                 echo '<a href="#">';
                 _e('<b>Unsubscribe</b> from this topic.', 'asgaros-forum');
                 echo '</a>';
+            } else {
+                // User has no subscription for this topic
+                echo '<a href="#">';
+                _e('<b>Subscribe</b> to this topic.', 'asgaros-forum');
+                echo '</a>';
             }
 
             echo '</div>';
+        }
+    }
+
+    // Checks if the current user has a subscription for the current topic
+    public static function isSubscribed($topic_id) {
+        $user_id = get_current_user_id();
+        $status = get_user_meta($user_id, 'asgarosforum_subscription_topic_'.$topic_id, true);
+
+        if (!empty($status)) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
