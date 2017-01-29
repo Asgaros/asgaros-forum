46f09c07bb118ae5b846454119e6e9da3d2b689f
Thomas <developer@thomasbelser.net>
2017-01-29 20:30:16 +0100

    Optimize breadcrumbs generation
    Optimize breadcrumbs generation.

--- includes/forum-breadcrumbs.php
+++ includes/forum-breadcrumbs.php
@@ -3,44 +3,57 @@
 if (!defined('ABSPATH')) exit;
 
 class AsgarosForumBreadCrumbs {
+    public static $breadCrumbsElements = 0;
+
     public static function showBreadCrumbs() {
         global $asgarosforum;
 
         if ($asgarosforum->options['show_breadcrumbs']) {
-            echo '<div id="breadcrumbs">';
-            echo '<span class="dashicons-before dashicons-admin-home"></span>';
-            echo '<a href="'.$asgarosforum->getLink('home').'">'.__('Forum', 'asgaros-forum').'</a>';
+            // Build breadcrumbs links.
+            $breadCrumsLinks = array();
+            $breadCrumsLinks[] = '<a href="'.$asgarosforum->getLink('home').'">'.__('Forum', 'asgaros-forum').'</a>';
 
             if ($asgarosforum->parent_forum && $asgarosforum->parent_forum > 0) {
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="'.$asgarosforum->getLink('forum', $asgarosforum->parent_forum).'">'.esc_html(stripslashes($asgarosforum->get_name($asgarosforum->parent_forum, $asgarosforum->tables->forums))).'</a>';
+                $breadCrumsLinks[] = '<a href="'.$asgarosforum->getLink('forum', $asgarosforum->parent_forum).'">'.esc_html(stripslashes($asgarosforum->get_name($asgarosforum->parent_forum, $asgarosforum->tables->forums))).'</a>';
             }
 
             if ($asgarosforum->current_forum) {
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="'.$asgarosforum->getLink('forum', $asgarosforum->current_forum).'">'.esc_html(stripslashes($asgarosforum->get_name($asgarosforum->current_forum, $asgarosforum->tables->forums))).'</a>';
+                $breadCrumsLinks[] = '<a href="'.$asgarosforum->getLink('forum', $asgarosforum->current_forum).'">'.esc_html(stripslashes($asgarosforum->get_name($asgarosforum->current_forum, $asgarosforum->tables->forums))).'</a>';
             }
 
             if ($asgarosforum->current_topic) {
                 $name = stripslashes($asgarosforum->get_name($asgarosforum->current_topic, $asgarosforum->tables->topics));
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="'.$asgarosforum->getLink('topic', $asgarosforum->current_topic).'" title="'.esc_html($name).'">'.esc_html($asgarosforum->cut_string($name)).'</a>';
+                $breadCrumsLinks[] = '<a href="'.$asgarosforum->getLink('topic', $asgarosforum->current_topic).'" title="'.esc_html($name).'">'.esc_html($asgarosforum->cut_string($name)).'</a>';
             }
 
             if ($asgarosforum->current_view === 'addpost') {
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="#">'.__('Post Reply', 'asgaros-forum').'</a>';
+                $breadCrumsLinks[] = '<a href="#">'.__('Post Reply', 'asgaros-forum').'</a>';
             } else if ($asgarosforum->current_view === 'editpost') {
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="#">'.__('Edit Post', 'asgaros-forum').'</a>';
+                $breadCrumsLinks[] = '<a href="#">'.__('Edit Post', 'asgaros-forum').'</a>';
             } else if ($asgarosforum->current_view === 'addtopic') {
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="#">'.__('New Topic', 'asgaros-forum').'</a>';
+                $breadCrumsLinks[] = '<a href="#">'.__('New Topic', 'asgaros-forum').'</a>';
             } else if ($asgarosforum->current_view === 'search') {
-                echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
-                echo '<a href="#">'.__('Search', 'asgaros-forum').'</a>';
+                $breadCrumsLinks[] = '<a href="#">'.__('Search', 'asgaros-forum').'</a>';
+            }
+
+            // Render breadcrums links.
+            echo '<div id="breadcrumbs">';
+            foreach ($breadCrumsLinks as $link) {
+                self::renderBreadCrumbsElement($link);
             }
             echo '</div>';
         }
+    }
+
+    public static function renderBreadCrumbsElement($link) {
+        if (self::$breadCrumbsElements === 0) {
+            echo '<span class="dashicons-before dashicons-admin-home"></span>';
+        } else {
+            echo '<span class="dashicons-before dashicons-arrow-right-alt2 separator"></span>';
+        }
+
+        echo $link;
+
+        self::$breadCrumbsElements++;
     }
 }
