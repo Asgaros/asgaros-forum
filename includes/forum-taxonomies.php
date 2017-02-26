591518f22a59aaa97ee5e6c56b5c438690dc4ad9
Thomas <developer@thomasbelser.net>
2017-02-26 11:24:00 +0100

    Prepare for v1.4.2 release
    Prepare for v1.4.2 release.

--- includes/forum-taxonomies.php
+++ includes/forum-taxonomies.php
@@ -8,24 +8,8 @@ class AsgarosForumTaxonomies {
             'asgarosforum-category',
             null,
             array(
-                'labels' => array(
-                    'name'          => __('Categories', 'asgaros-forum'),
-                    'singular_name' => __('Category', 'asgaros-forum'),
-                    'edit_item'     => __('Edit Category', 'asgaros-forum'),
-                    'update_item'   => __('Update Category', 'asgaros-forum'),
-                    'add_new_item'  => __('Add new Category', 'asgaros-forum'),
-                    'search_items'  => __('Search Categories', 'asgaros-forum'),
-                    'not_found'     => __('No Categories found.', 'asgaros-forum')
-                ),
                 'public' => false,
-                'show_ui' => true,
-                'rewrite' => false,
-                'capabilities' => array(
-                    'manage_terms' => 'manage_options',
-					'edit_terms'   => 'manage_options',
-					'delete_terms' => 'manage_options',
-					'assign_terms' => 'manage_options'
-				)
+                'rewrite' => false
             )
         );
 	}
