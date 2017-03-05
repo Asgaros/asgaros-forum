dc4ce4cf31c372032c8ffc0645671bfe28da8a8b
Thomas <developer@thomasbelser.net>
2017-03-05 13:49:20 +0100

    More little design and structure changes
    More little design and structure changes.

--- includes/forum.php
+++ includes/forum.php
@@ -616,10 +616,6 @@ class AsgarosForum {
     function post_menu($post_id, $author_id, $counter) {
         $o = '';
 
-        if ((!is_user_logged_in() && $this->options['allow_guest_postings'] && !$this->get_status('closed')) || (is_user_logged_in() && (!$this->get_status('closed') || AsgarosForumPermissions::isModerator('current')) && !AsgarosForumPermissions::isBanned('current'))) {
-            $o .= '<a class="forum-editor-quote-button" data-value-id="'.$post_id.'" href="'.$this->getLink('post_add', $this->current_topic, array('quote' => $post_id)).'"><span class="dashicons-before dashicons-editor-quote"></span>'.__('Quote', 'asgaros-forum').'</a>';
-        }
-
         if (is_user_logged_in()) {
             if (($counter > 1 || $this->current_page >= 1) && AsgarosForumPermissions::isModerator('current')) {
                 $o .= '<a onclick="return confirm(\''.__('Are you sure you want to remove this?', 'asgaros-forum').'\');" href="'.$this->getLink('topic', $this->current_topic, array('post' => $post_id, 'remove_post' => 1)).'"><span class="dashicons-before dashicons-trash"></span>'.__('Delete', 'asgaros-forum').'</a>';
@@ -628,6 +624,10 @@ class AsgarosForum {
             if ((AsgarosForumPermissions::isModerator('current') || get_current_user_id() == $author_id) && !AsgarosForumPermissions::isBanned('current')) {
                 $o .= '<a href="'.$this->getLink('post_edit', $post_id, array('part' => ($this->current_page + 1))).'"><span class="dashicons-before dashicons-edit"></span>'.__('Edit', 'asgaros-forum').'</a>';
             }
+        }
+
+        if ((!is_user_logged_in() && $this->options['allow_guest_postings'] && !$this->get_status('closed')) || (is_user_logged_in() && (!$this->get_status('closed') || AsgarosForumPermissions::isModerator('current')) && !AsgarosForumPermissions::isBanned('current'))) {
+            $o .= '<a class="forum-editor-quote-button" data-value-id="'.$post_id.'" href="'.$this->getLink('post_add', $this->current_topic, array('quote' => $post_id)).'"><span class="dashicons-before dashicons-editor-quote"></span>'.__('Quote', 'asgaros-forum').'</a>';
         }
 
         $o = (!empty($o)) ? $o = '<div class="post-menu">'.$o.'</div>' : $o;
