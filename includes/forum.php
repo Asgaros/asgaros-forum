d5bf837ecfd5a6a1a8692e78533a6a4d93f09b00
Thomas <developer@thomasbelser.net>
2017-05-27 11:30:15 +0200

    Prepare for v1.5.2 release
    Prepare for v1.5.2 release

--- includes/forum.php
+++ includes/forum.php
@@ -3,7 +3,7 @@
 if (!defined('ABSPATH')) exit;
 
 class AsgarosForum {
-    var $version = '1.5.1';
+    var $version = '1.5.2';
     var $executePlugin = false;
     var $db = null;
     var $tables = null;
