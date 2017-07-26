f0e2d6e85f6b893a5475f92b9a942792f0b765b7
Thomas <developer@thomasbelser.net>
2017-07-27 00:28:28 +0200

    Prepare for v1.5.7 release

--- includes/forum.php
+++ includes/forum.php
@@ -3,7 +3,7 @@
 if (!defined('ABSPATH')) exit;
 
 class AsgarosForum {
-    var $version = '1.5.6';
+    var $version = '1.5.7';
     var $executePlugin = false;
     var $db = null;
     var $tables = null;
