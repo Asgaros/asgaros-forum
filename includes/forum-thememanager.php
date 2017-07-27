60787213fea526941243b325beea04ac87fcb726
Thomas <developer@thomasbelser.net>
2017-07-27 10:26:36 +0200

    Enqueue CSS during wp_enqueue_scripts action

--- includes/forum-thememanager.php
+++ includes/forum-thememanager.php
@@ -31,6 +31,7 @@ class AsgarosForumThemeManager {
 		}
 
 		add_filter('mce_css', array($this, 'addEditorCSS'));
+		add_action('wp_enqueue_scripts', array($this, 'addCSS'));
 		add_action('wp_head', array($this, 'setHeader'));
 	}
 
@@ -113,24 +114,27 @@ class AsgarosForumThemeManager {
 			echo '<meta name="twitter:description" content="'.$currentDescription.'" />'.PHP_EOL;
 		}
 
+		echo '<!-- Asgaros Forum: END -->'.PHP_EOL;
+	}
+
+	public function addCSS() {
 		$themeurl = self::get_current_theme_url();
 
-         	wp_enqueue_style( 'af-widgets', $themeurl.'/widgets.css',  array(), self::$asgarosforum->version, '' );
+		wp_enqueue_style('af-widgets', $themeurl.'/widgets.css', array(), self::$asgarosforum->version);
+
 		if (self::$asgarosforum->executePlugin) {
-	        wp_enqueue_style( 'af-style', $themeurl.'/style.css',  array(), self::$asgarosforum->version, '' );
+			wp_enqueue_style('af-style', $themeurl.'/style.css', array(), self::$asgarosforum->version);
 
-	        if (self::is_default_theme()) {
-	            if ((self::$asgarosforum->options['custom_color'] !== self::$asgarosforum->options_default['custom_color']) || (self::$asgarosforum->options['custom_text_color'] !== self::$asgarosforum->options_default['custom_text_color']) || (self::$asgarosforum->options['custom_background_color'] !== self::$asgarosforum->options_default['custom_background_color'])) {
-	                wp_enqueue_style( 'af-custom-color', $themeurl.'/custom-color.php?color='.substr(self::$asgarosforum->options['custom_color'], 1).'&amp;text-color='.substr(self::$asgarosforum->options['custom_text_color'], 1).'&amp;background-color='.substr(self::$asgarosforum->options['custom_background_color'], 1),  array(), self::$asgarosforum->version, '' );
-	            }
-	        }
+			if (self::is_default_theme()) {
+				if ((self::$asgarosforum->options['custom_color'] !== self::$asgarosforum->options_default['custom_color']) || (self::$asgarosforum->options['custom_text_color'] !== self::$asgarosforum->options_default['custom_text_color']) || (self::$asgarosforum->options['custom_background_color'] !== self::$asgarosforum->options_default['custom_background_color'])) {
+					wp_enqueue_style('af-custom-color', $themeurl.'/custom-color.php?color='.substr(self::$asgarosforum->options['custom_color'], 1).'&amp;text-color='.substr(self::$asgarosforum->options['custom_text_color'], 1).'&amp;background-color='.substr(self::$asgarosforum->options['custom_background_color'], 1), array(), self::$asgarosforum->version);
+				}
+			}
 
-	        if (wp_is_mobile()) {
-	            wp_enqueue_style( 'af-mobile', $themeurl.'/mobile.css',  array(), self::$asgarosforum->version, '' );
-	        }
+			if (wp_is_mobile()) {
+				wp_enqueue_style('af-mobile', $themeurl.'/mobile.css', array(), self::$asgarosforum->version);
+			}
 		}
-
-		echo '<!-- Asgaros Forum: END -->'.PHP_EOL;
 	}
 
 	// Add a custom stylesheet to the TinyMCE editor.
--- readme.txt
+++ readme.txt
@@ -125,6 +125,7 @@ You can add own themes for your forum in the `/wp-content/themes-asgarosforum` d
 6. Manage general options.
 
 == Changelog ==
+* Changed: Enqueue stylesheets
 = 1.5.7 =
 * Added: Subscription overview
 * Fixed: Strip slashes when showing the description inside of a forum
