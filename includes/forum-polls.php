9cf6619a2298eb8903e10cc153cc0071d8a85538
Asgaros <developer@thomasbelser.net>
2019-03-07 23:14:42 +0100

    Show poll-form only when adding a new topic #195

--- includes/forum-polls.php
+++ includes/forum-polls.php
@@ -8,12 +8,17 @@ class AsgarosForumPolls {
     public function __construct($object) {
         $this->asgarosforum = $object;
 
-        add_action('asgarosforum_editor_custom_content_bottom', array($this, 'render_editor_form'));
+        add_action('asgarosforum_editor_custom_content_bottom', array($this, 'render_poll_form'), 10, 1);
     }
 
-    public function render_editor_form() {
+    public function render_poll_form($editor_view) {
         // Cancel if poll-functionality is disabled.
         if (!$this->asgarosforum->options['enable_polls']) {
+            return;
+        }
+
+        // Cancel if we are not in the addtopic editor-view.
+        if ($editor_view !== 'addtopic') {
             return;
         }
 
@@ -22,16 +27,16 @@ class AsgarosForumPolls {
 
             echo '<div id="poll-form">';
                 echo '<div id="poll-question">';
-                    echo '<input class="editor-subject-input" type="text" maxlength="255" name="poll-question-input" placeholder="'.__('Will you enter a question here?', 'asgaros-forum').'" value="">';
+                    echo '<input class="editor-subject-input" type="text" maxlength="255" name="poll-title" placeholder="'.__('Will you enter a question here?', 'asgaros-forum').'" value="">';
                 echo '</div>';
 
                 echo '<div id="poll-options">';
-                    echo '<input class="editor-subject-input" type="text" maxlength="255" name="poll-question-option[]" placeholder="'.__('Yes', 'asgaros-forum').'" value="">';
-                    echo '<input class="editor-subject-input" type="text" maxlength="255" name="poll-question-option[]" placeholder="'.__('No', 'asgaros-forum').'" value="">';
+                    echo '<input class="editor-subject-input" type="text" maxlength="255" name="poll-option[]" placeholder="'.__('Yes', 'asgaros-forum').'" value="">';
+                    echo '<input class="editor-subject-input" type="text" maxlength="255" name="poll-option[]" placeholder="'.__('No', 'asgaros-forum').'" value="">';
                 echo '</div>';
 
                 echo '<label class="editor-label">';
-                    echo '<input type="checkbox" name="poll-multiple-answers"><span>'.__('Allow multiple answers', 'asgaros-forum').'</span>';
+                    echo '<input type="checkbox" name="poll-multiple"><span>'.__('Allow multiple answers', 'asgaros-forum').'</span>';
                 echo '</label>';
             echo '</div>';
         echo '</div>';
