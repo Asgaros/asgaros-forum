449eed30f0ac4628e15e553f8fb6f1a830c6adf6
Asgaros <developer@thomasbelser.net>
2019-05-07 22:44:38 +0200

    Prepare for v1.14.5 release

--- includes/forum.php
+++ includes/forum.php
@@ -3,7 +3,7 @@
 if (!defined('ABSPATH')) exit;
 
 class AsgarosForum {
-    var $version = '1.14.4';
+    var $version = '1.14.5';
     var $executePlugin = false;
     var $db = null;
     var $tables = null;
