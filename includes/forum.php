c934251609319225a63d58090ff0ed2b15d4b965
Asgaros <developer@thomasbelser.net>
2023-03-16 12:47:16 +0800

    Prepare for v2.4.1 release

--- includes/forum.php
+++ includes/forum.php
@@ -3,7 +3,7 @@
 if (!defined('ABSPATH')) exit;
 
 class AsgarosForum {
-    public $version = '2.4.0';
+    public $version = '2.4.1';
     public $executePlugin = false;
     public $db = null;
     public $tables = null;
