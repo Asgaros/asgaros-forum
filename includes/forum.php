693304b66ceb8bee1db22427bc4b649420cd6861
Asgaros <developer@thomasbelser.net>
2024-01-19 10:55:19 +0800

    Prepare for v2.8.0 release

--- includes/forum.php
+++ includes/forum.php
@@ -5,7 +5,7 @@ if (!defined('ABSPATH')) {
 }
 
 class AsgarosForum {
-    public $version               = '2.7.2';
+    public $version               = '2.8.0';
     public $executePlugin         = false;
     public $db                    = null;
     public $tables                = null;
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: forum, forums, discussion, multisite, community, bulletin, board, asgaros,
 Requires at least: 4.9
 Tested up to: 6.4
 Requires PHP: 5.3
-Stable tag: 2.7.2
+Stable tag: 2.8.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
